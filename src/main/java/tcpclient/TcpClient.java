569a93a94654ebbf30c6bf067f6bb2003c300bed
Alex K <the3mp3@gmail.com>
2018-06-28 09:36:33 +0300

    first commit with basic client for testing

---
+++ src/main/java/tcpclient/TcpClient.java
@@ -0,0 +1,34 @@
+package tcpclient;
+
+import java.io.*;
+import java.net.Socket;
+
+public class TcpClient {
+    public static void main(String [] args) {
+
+        try (Socket clientSocket = new Socket("localhost", 12345);
+             InputStream inputStream = clientSocket.getInputStream();
+             OutputStream outputStream = clientSocket.getOutputStream();
+             InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+             BufferedReader inFromUser = new BufferedReader(inputStreamReader);
+             DataInputStream inFromServer = new DataInputStream(inputStream);
+             DataOutputStream outToServer = new DataOutputStream(outputStream)
+        ){
+            while (true) {
+                System.out.print("Command : ");
+                try {
+                    String temp = inFromUser.readLine();
+                    outToServer.writeUTF(temp);
+                    outToServer.flush();
+
+                    System.out.print(inFromServer.readUTF());
+                } catch (Exception e) {
+                    System.out.println("Got an exception after command: " + e);
+                    throw new Exception(e);
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("Got an exception when opening client socket. " + ex);
+        }
+    }
+}
